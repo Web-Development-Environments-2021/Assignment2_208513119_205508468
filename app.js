722d1e8b4715be8588216de5e4a8f20b4121c85e
DanaKlim <dana1997k@gmail.com>
2021-04-17 17:22:32 +0300

    usersDB functionality

--- app.js
+++ app.js
@@ -204,55 +204,30 @@ function UpdatePosition() {
 	}
 }
 
-function checkValidPassword(psw){
-	// if(psw.length < 6){
-	// 	alert("password not valid - should include at least 6 characters: letters & numbers only"); 
-	// 	return false; 
-	// }
-	var letterNumber = /^(?=.*\d)(?=.*[a-zA-Z]).{6,}$/;
-	if(psw.match(letterNumber)) 
-	 {
-	  return true;
-	 }
-	alert("password not valid - should include at least 6 characters: letters & numbers only"); 
-	return false; 
-	 
-}
-
-function checkValidFullName(fullname){
-	let ans = /\d/.test(fullname);
-	if (ans===true){
-		alert("Full Name not valid - should not include numbers"); 
-		return false; 
-	}
-	return true;
-}
-
-function checkValidMail(email){
-	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    let ans = re.test(String(email).toLowerCase());
-	if (ans === false){
-		alert("Mail not Valid - try Again"); 
-		return false; 
-	}
-	return true;
-}
-
 function registerUser(){
 	let userName = document.getElementById('usernameReg').value;
+	// let existUserName = checkExistUserName(userName);
 	let psw = document.getElementById('pswReg').value;
-	let fullName = document.getElementById('nameReg').value;
-	let email = document.getElementById('emailReg').value;
-	let birthDate = document.getElementById('dateofbirthReg').value;
+	let fullname = document.getElementById('nameReg').value;
+	let mail = document.getElementById('emailReg').value;
+	let birthdate = document.getElementById('dateofbirthReg').value;
 	let validPassword = checkValidPassword(psw);
-	let validFullName = checkValidFullName(fullName);
-	let validMail = checkValidMail(email);
+	let validFullName = checkValidFullName(fullname);
+	let validMail = checkValidMail(mail);
 	if(validPassword && validFullName && validMail){
-		users.push({userName, psw,fullName, email, birthDate});
+		users.push({
+			username: userName,
+			password: psw,
+			fullName: fullname,
+			email: mail,
+			birthDate:birthdate
+		});
 		if (confirm('Registration Succeed ! Do you want to start a game right now ?')) {
 			// start game 
+			showGame();
 		  } else {
 			// don't start game!
+			showWelcome();
 		  }
 	}		
 }
--- usersDB.js
+++ usersDB.js
@@ -1,15 +1,24 @@
 const users = [
     {
         username: "k",
-        password: "k"
+        password: "k",
+        fullName: "",
+        email: "",
+        birthDate:""
     },
     {
         username: "roy",
-        password: "roy123"
+        password: "roy123",
+        fullName: "",
+        email: "",
+        birthDate:""
     },
     {
         username: "dana",
-        password: "dana123"
+        password: "dana123",
+        fullName: "",
+        email: "",
+        birthDate:""
     }
 ];
 
@@ -36,4 +45,38 @@ function signIn() {
     else {
         alert("Username or Password is not correct!\nPlease Try Again!")
     }
+}
+
+function checkValidPassword(psw){
+	// if(psw.length < 6){
+	// 	alert("password not valid - should include at least 6 characters: letters & numbers only"); 
+	// 	return false; 
+	// }
+	var letterNumber = /^(?=.*\d)(?=.*[a-zA-Z]).{6,}$/;
+	if(psw.match(letterNumber)) 
+	 {
+	  return true;
+	 }
+	alert("password not valid - should include at least 6 characters: letters & numbers only"); 
+	return false; 
+	 
+}
+
+function checkValidFullName(fullname){
+	let ans = /\d/.test(fullname);
+	if (ans===true){
+		alert("Full Name not valid - should not include numbers"); 
+		return false; 
+	}
+	return true;
+}
+
+function checkValidMail(email){
+	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    let ans = re.test(String(email).toLowerCase());
+	if (ans === false){
+		alert("Mail not Valid - try Again"); 
+		return false; 
+	}
+	return true;
 }
